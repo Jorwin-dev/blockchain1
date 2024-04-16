9744788b8d1ba3f998b8a787f3673813d13f1303
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2024-04-16 16:19:38 -0600

    Allow creation of new Blocks

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,36 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def _init_(self):
         self.chain = []
         self.current_transactions = []
-        
-    def new_block(self):
-        # creates a new Block and adds it to the chain pass
 
+        # create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+        
+    def new_block(self, proof, previous_hash=None):
+        """"
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) +1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        # reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+    
     def new_transactions(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
@@ -23,11 +48,19 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # hashes a Block
-        pass
-
     @property
     def last_block(self):
         # returns the last Block in the chain pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # make sure that Dictionary is ordered
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
