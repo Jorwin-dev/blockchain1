551228ab6188b950880409d1856b3280d9d86f22
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2024-04-16 17:15:10 -0600

    Implement Proof of Work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def _init_(self):
@@ -64,3 +66,31 @@ class Blockchain(object):
         # make sure that Dictionary is ordered
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+    
+    def proof_of_work(self, last_proof):
+        """
+        Proof of Work Algorithm:
+        - Find a number p' such that hash(pp') contains leading 4 zeros, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeros?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
